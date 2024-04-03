c906d85533539f971ef3c04314e8b61d0e50e7ec
An <phamtrongan2710@gmail.com>
2024-04-03 23:15:36 +0700

    test

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-});
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres',
+//     logging: false,
+// });
+
+const sequelize = new Sequelize(process.env.DB_CONN_STR)
 
 module.exports = sequelize;
