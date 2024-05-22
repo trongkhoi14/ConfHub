6cc489426777444160364175b57c89be83e23dd6
An <phamtrongan2710@gmail.com>
2024-05-22 23:40:09 +0700

    trans

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -8,7 +8,8 @@ const initSocket = (server) => {
         cors: {
             origin: '*',
         },
-        path: '/'
+        path: '/',
+        transports: ['websocket', 'polling']
     });
     return io;
 };
