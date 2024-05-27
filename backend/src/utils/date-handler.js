a2f223d82dd553ea4d19c82d1f771483da785dab
An <phamtrongan2710@gmail.com>
2024-05-27 15:52:44 +0700

    format date y/m/d

--- backend/src/utils/date-handler.js
+++ backend/src/utils/date-handler.js
@@ -3,5 +3,5 @@ module.exports.formatDate = (stringDate) => {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${year}/${month}/${day}`;
 }
